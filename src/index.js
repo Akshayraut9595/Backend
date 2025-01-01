891145c95017feb40a5993734e94b72b4bdd6618
Akshayraut9595 <akshayraut8080@gmail.com>
2025-01-01 14:45:22 +0530

    connection to mongDB

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+dotenv.config({
+    path:'.env'
+});
+
+// import express from "express"
+// const app = express();
+// import { DB_NAME } from "./constants";
+// import mongoose from "mongoose";
+
+// async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log("Error: ", error);
+//       throw error;
+//     });
+
+//     app.listen(process.env.PORT, () => {
+//       console.log(`App is listening on port ${process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.log("Error: ", error);
+//     throw error;
+//   }
+// };
+
+connectDB();
